fa0e7739705a5c04a6515138c07a60a97b777e0b
Bhargav7322 <bhargav.meva@gmail.com>
2026-08-20 12:18:48 +0530

    Add Routes And intial setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,33 @@
-import React from 'react'
+import { Toaster } from "react-hot-toast"
+import { Navigate, Route, Routes } from "react-router-dom"
+import LoginLanding from "./pages/LoginLanding"
+import Layout from "./pages/Layout"  
+import Dashboard from "./pages/Dashboard"
+import Settings from "./pages/Settings"
+import Employees from "./pages/Employees"
+import Attendance from "./pages/Attendance"
+import Leave from "./pages/Leave"
+import Payslips from "./pages/Payslips"
+import PrintPayslips from "./pages/PrintPayslips"
 
 const App = () => {
   return (
-    <div>
-      App is running
-    </div>
+   <>
+   <Toaster/>
+   <Routes>
+    <Route path="/login" element={<LoginLanding />} />
+    <Route element={<Layout />} >
+    <Route path="/dashboard" element={<Dashboard />} />
+    <Route path="/employees" element={<Employees />} />
+    <Route path="/attendance" element={<Attendance />} />
+    <Route path="/leave" element={<Leave />} />
+    <Route path="/payslips" element={<Payslips />} />
+    <Route path="/settings" element={<Settings />} />
+    </Route>
+    <Route path="/print/payslips/:id" element={<PrintPayslips />} />
+    <Route path="*" element={<Navigate to="/dashboard" replace />} />
+   </Routes>
+   </>
   )
 }
 
